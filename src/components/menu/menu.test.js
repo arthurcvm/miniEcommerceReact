2621b0463dd91ab197078f95c18b07c11438a059
Arthur Cezar <arthurcvm@hotmail.com>
2021-11-23 22:08:18 -0300

    test: implements test to render menu text

---
+++ src/components/menu/menu.test.js
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react'
+import Menu from './menu'
+
+describe('Teste do componente de menu', () => {
+    it('deve renderizar o componente sem erros', () => {
+        const { getByText } = render(<Menu />);
+        const texto = getByText(/menu/i);
+        expect(texto).toBeInTheDocument();
+    })
+})
